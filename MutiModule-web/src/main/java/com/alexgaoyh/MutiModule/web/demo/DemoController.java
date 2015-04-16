b0c82a515144ed98bd4bf128ce117497fef43884
alexgaoyh <alexgaoyh@sina.com>
2015-04-16 12:32:34 +0800

    增加web模块，springmvc的controller部分，实现页面的显示部分

---
+++ MutiModule-web/src/main/java/com/alexgaoyh/MutiModule/web/demo/DemoController.java
@@ -0,0 +1,37 @@
+package com.alexgaoyh.MutiModule.web.demo;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.alexgaoyh.MutiModule.domain.demo.DemoEntity;
+import com.alexgaoyh.MutiModule.service.demo.IDemoService;
+
+@Controller
+@RequestMapping(value="demo")
+public class DemoController {
+	
+	@Resource
+	private IDemoService demoService;
+
+	//demoService get/set 方法  begin
+	public IDemoService getDemoService() {
+		return demoService;
+	}
+
+	public void setDemoService(IDemoService demoService) {
+		this.demoService = demoService;
+	}
+	//demoService get/set 方法  end
+	
+
+	@RequestMapping("index")
+	public String index() {
+		DemoEntity demo = new DemoEntity();
+		demo.setName("zz");
+		demoService.insertDemo(demo);
+		return "demo/index";
+	}
+	
+}
