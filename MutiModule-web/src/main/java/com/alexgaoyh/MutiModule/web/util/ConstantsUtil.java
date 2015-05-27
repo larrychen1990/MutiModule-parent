2ca8339875b822603257a053d0c9fdf1ac541173
alexgaoyh <alexgaoyh@sina.com>
2015-05-27 11:16:43 +0800

    常量修改

--- MutiModule-web/src/main/java/com/alexgaoyh/MutiModule/web/util/ConstantsUtil.java
+++ MutiModule-web/src/main/java/com/alexgaoyh/MutiModule/web/util/ConstantsUtil.java
@@ -15,15 +15,15 @@ public class ConstantsUtil {
 	/**
 	 * 用户登录后，将用户信息保存时对应的常量信息
 	 */
-	public static final String ADMIN_LOGIN_CONSTANTS = "admin_login_constants_";
+	public static final String ADMIN_LOGIN_CONSTANTS = "admin_login_constants";
 	
 	/**
 	 * redis 方面，key的过期时间，一分钟
 	 */
-	public static final int EXPIRE_TIME_1_MINUTE = 1;
+	public static final int EXPIRE_TIME_1_MINUTE = 1 * 60;
 	
 	/**
 	 * redis 方面。key的过期时间，三十分钟
 	 */
-	public static final int EXPIRE_TIME_30_MINUTE = 30;
+	public static final int EXPIRE_TIME_30_MINUTE = 30 * 60;
 }
