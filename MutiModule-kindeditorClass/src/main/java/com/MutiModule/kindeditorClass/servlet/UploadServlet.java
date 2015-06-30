71f799084bd6c28d7fda6499643dddf21b315231
alexgaoyh <alexgaoyh@sina.com>
2015-06-30 10:42:17 +0800

    增加kindeditor类似的实现

---
+++ MutiModule-kindeditorClass/src/main/java/com/MutiModule/kindeditorClass/servlet/UploadServlet.java
@@ -0,0 +1,146 @@
+package com.MutiModule.kindeditorClass.servlet;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.json.simple.JSONObject;
+
+public class UploadServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		//文件保存目录路径
+		String savePath = req.getServletContext().getRealPath("/") + "attached/";
+
+		//文件保存目录URL
+		String saveUrl  = req.getContextPath() + "/attached/";
+
+		//定义允许上传的文件扩展名
+		HashMap<String, String> extMap = new HashMap<String, String>();
+		extMap.put("image", "gif,jpg,jpeg,png,bmp");
+		extMap.put("flash", "swf,flv");
+		extMap.put("media", "swf,flv,mp3,wav,wma,wmv,mid,avi,mpg,asf,rm,rmvb");
+		extMap.put("file", "doc,docx,xls,xlsx,ppt,htm,html,txt,zip,rar,gz,bz2");
+
+		//最大文件大小
+		long maxSize = 1000000;
+
+		resp.setContentType("text/html; charset=UTF-8");
+
+		if(!ServletFileUpload.isMultipartContent(req)){
+			resp.getWriter().println(getError("请选择文件。"));
+			return;
+		}
+		//检查目录
+		File uploadDir = new File(savePath);
+		if(!uploadDir.isDirectory()){
+			resp.getWriter().println(getError("上传目录不存在。"));
+			return;
+		}
+		//检查目录写权限
+		if(!uploadDir.canWrite()){
+			resp.getWriter().println(getError("上传目录没有写权限。"));
+			return;
+		}
+
+		String dirName = req.getParameter("dir");
+		if (dirName == null) {
+			dirName = "image";
+		}
+		if(!extMap.containsKey(dirName)){
+			resp.getWriter().println(getError("目录名不正确。"));
+			return;
+		}
+		//创建文件夹
+		savePath += dirName + "/";
+		saveUrl += dirName + "/";
+		File saveDirFile = new File(savePath);
+		if (!saveDirFile.exists()) {
+			saveDirFile.mkdirs();
+		}
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+		String ymd = sdf.format(new Date());
+		savePath += ymd + "/";
+		saveUrl += ymd + "/";
+		File dirFile = new File(savePath);
+		if (!dirFile.exists()) {
+			dirFile.mkdirs();
+		}
+
+		FileItemFactory factory = new DiskFileItemFactory();
+		ServletFileUpload upload = new ServletFileUpload(factory);
+		upload.setHeaderEncoding("UTF-8");
+		try {
+			List<FileItem> items = upload.parseRequest(req);
+			Iterator itr = items.iterator();
+			while (itr.hasNext()) {
+				FileItem item = (FileItem) itr.next();
+				String fileName = item.getName();
+				long fileSize = item.getSize();
+				if (!item.isFormField()) {
+					//检查文件大小
+					if(item.getSize() > maxSize){
+						resp.getWriter().println(getError("上传文件大小超过限制。"));
+						return;
+					}
+					//检查扩展名
+					String fileExt = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
+					if(!Arrays.<String>asList(extMap.get(dirName).split(",")).contains(fileExt)){
+						resp.getWriter().println(getError("上传文件扩展名是不允许的扩展名。\n只允许" + extMap.get(dirName) + "格式。"));
+						return;
+					}
+
+					SimpleDateFormat df = new SimpleDateFormat("yyyyMMddHHmmss");
+					String newFileName = df.format(new Date()) + "_" + new Random().nextInt(1000) + "." + fileExt;
+					try{
+						File uploadedFile = new File(savePath, newFileName);
+						item.write(uploadedFile);
+					}catch(Exception e){
+						resp.getWriter().println(getError("上传文件失败。"));
+						return;
+					}
+
+					JSONObject obj = new JSONObject();
+					obj.put("error", 0);
+					obj.put("url", saveUrl + newFileName);
+					resp.getWriter().println(obj.toJSONString());
+				}
+			}
+		} catch (FileUploadException e1) {
+			resp.getWriter().println(getError("FileUploadException"));
+			return;
+		}
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
+	private String getError(String message) {
+		JSONObject obj = new JSONObject();
+		obj.put("error", 1);
+		obj.put("message", message);
+		return obj.toJSONString();
+	}
+}
---
+++ MutiModule-kindeditorDemo/README.md
@@ -0,0 +1,5 @@
+#20150610
+	实现与  MutiModule-ueditorDemo 相似的功能；
+	添加新的富文本编辑器的功能  kindeditor  https://github.com/kindsoft/kindeditor 
+	
+	
